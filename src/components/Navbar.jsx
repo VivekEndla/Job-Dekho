cebc437e1b30c6fb3c8d6cd12e41ddb481ca7803
VivekEndla <endlavivek81@gmail.com>
2025-08-22 23:12:40 +0530

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Make sure Bootstrap JS is imported
 
 const Navbar = ({ isLoggedIn, setIsLoggedIn }) => {
   const navigate = useNavigate();
+  const collapseRef = useRef(null);
 
   const handleLog = () => {
     if (isLoggedIn) {
@@ -12,17 +14,25 @@ const Navbar = ({ isLoggedIn, setIsLoggedIn }) => {
     } else {
       navigate('/login');
     }
+    closeNavbar();
+  };
+
+  const closeNavbar = () => {
+    const collapse = collapseRef.current;
+    if (collapse && collapse.classList.contains('show')) {
+      // Bootstrap 5 collapse toggle
+      const bsCollapse = window.bootstrap.Collapse.getInstance(collapse);
+      bsCollapse?.hide();
+    }
   };
 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm sticky-top">
       <div className="container">
-        {/* Brand */}
         <NavLink to="/" className="navbar-brand d-flex align-items-center fw-bold fs-3 text-danger">
           JOBDEKHO
         </NavLink>
 
-        {/* Toggler */}
         <button
           className="navbar-toggler"
           type="button"
@@ -35,47 +45,42 @@ const Navbar = ({ isLoggedIn, setIsLoggedIn }) => {
           <span className="navbar-toggler-icon" />
         </button>
 
-        {/* Collapsible content */}
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup" ref={collapseRef}>
           <ul className="navbar-nav mx-auto mb-2 mb-lg-0 gap-3">
             <li className="nav-item">
-              <NavLink className="nav-link" to="/">
+              <NavLink className="nav-link" to="/" onClick={closeNavbar}>
                 Home
               </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to="/about">
+              <NavLink className="nav-link" to="/about" onClick={closeNavbar}>
                 About
               </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to={isLoggedIn ? '/Joblisting' : '/login'}>
+              <NavLink
+                className="nav-link"
+                to={isLoggedIn ? '/Joblisting' : '/login'}
+                onClick={closeNavbar}
+              >
                 Findjob
               </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to="/contact">
+              <NavLink className="nav-link" to="/contact" onClick={closeNavbar}>
                 Contact
               </NavLink>
             </li>
           </ul>
 
-          {/* Employee Button */}
-          <div className='d-flex gap-2 mt-3 mt-lg-0'>
-            <div >
-            <NavLink className="btn btn-outline-light" to="/Eregister">
+          <div className="d-flex gap-2 mt-3 mt-lg-0">
+            <NavLink className="btn btn-outline-light" to="/Eregister" onClick={closeNavbar}>
               Employee
             </NavLink>
-          </div>
-
-          {/* Login / Logout Button */}
-          <div >
             <button className="btn btn-outline-light" onClick={handleLog}>
               {isLoggedIn ? 'Logout' : 'Login'}
             </button>
           </div>
-          </div>
-          
         </div>
       </div>
     </nav>
